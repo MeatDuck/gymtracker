6942c3095cdc07973583011605afb179f4e70bde
khomchenko <khomchenko@netcracker.com>
2018-10-08 14:10:47 +0400

    init

---
+++ src/main/java/com/hompage/gymtracker/GymtrackerApplication.java
@@ -0,0 +1,12 @@
+package com.hompage.gymtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymtrackerApplication.class, args);
+	}
+}
